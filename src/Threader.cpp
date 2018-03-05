50fb512fe37c49fe39924e21b6c5da95d5bb297a
Adrianna <adriannabisikiewicz@gmail.com>
2018-03-05 16:25:14 +0100

    First moving object added

--- src/Threader.cpp
+++ src/Threader.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
 #include <thread>
 #include <ncurses.h>
+#include <unistd.h>
 
 using namespace std;
+#define DELAY 60000
 
-void hello_world()
+void make_ball(int y)
 {
-  printw("Hello from thread!\n");
-  refresh();
-  getch();
+  int x = 0;
+  int max_y = 0, max_x = 0;
+  int next_x = 0;
+  int direction = 1;
+
+  getmaxyx(stdscr, max_y, max_x);
+
+  while (1)
+  {
+    clear();
+    mvprintw(y, x, "o");
+    refresh();
+
+    usleep(DELAY);
+
+    next_x = x + direction;
+
+    if (next_x >= max_x || next_x < 0)
+    {
+      direction *= -1;
+    }
+    else
+    {
+      x += direction;
+    }
+  }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
   initscr();
-  thread t(hello_world);
-  t.join();
+  noecho();
+  curs_set(FALSE);
+
+  make_ball(0);
+
   endwin();
-  return 0;
 }
